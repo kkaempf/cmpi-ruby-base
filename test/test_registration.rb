efc4cd6d63194f91aae6436ec3ce708d26088ac4
Klaus Kämpf <kkaempf@suse.de>
2013-04-05 11:23:43 +0200

    Fix registration test, add DeepInheritance

--- test/test_registration.rb
+++ test/test_registration.rb
@@ -17,17 +17,17 @@ class Test_Registration < Test::Unit::TestCase
 
   def test_registered
     # all these class names must appear once in enumerate class names
-    classes = [ "RCP_ComputerSystem.mof",
-      "RCP_OperatingSystem.mof",
-      "RCP_OSProcess.mof",
-      "RCP_PhysicalMemory.mof",
-      "RCP_Processor.mof",
-      "RCP_RunningOS.mof",
-      "RCP_UnixProcess.mof"
+    classes = [ "RCP_ComputerSystem",
+      "RCP_OperatingSystem",
+      "RCP_OSProcess",
+      "RCP_PhysicalMemory",
+      "RCP_Processor",
+      "RCP_RunningOS",
+      "RCP_UnixProcess"
     ]
     
-    @client.class_names(@op).each do |name|
-      assert classes.delete(name)
+    @client.class_names(@op, Sfcc::Flags::DeepInheritance).each do |name|
+      classes.delete(name.to_s)
     end
     assert_equal [], classes
   end
